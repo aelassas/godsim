e621cafe09e54ef2b4cb2a357cfa7a98d4421d3a
aelassas <akram.elassas@gmail.com>
2025-09-13 00:10:37 +0100

    ci: update deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          mui: ['@mui/material', '@mui/icons-material'],
+          highlighter: ['react-syntax-highlighter'],
+        },
+      },
+    },
+  },
 })
